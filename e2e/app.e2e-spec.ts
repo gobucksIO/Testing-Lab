e62c01c67db6dce9fcaeaaa434e5a4322a9850e0
gobucksIO <tklax831@gmail.com>
2017-04-05 18:32:01 -0400

    CLI testing commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-testing App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('!');
   });
 });
